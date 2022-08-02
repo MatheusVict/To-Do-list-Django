6c3e941fa753010f5069cf955ad397cfa7c99acf
Matheus Vict <matheusvictorhenrique@gmail.com>
2022-08-02 11:27:22 -0300

    django-on_heroku

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,7 @@
 from telnetlib import STATUS
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.deletion import CASCADE
 
 
 class task(models.Model):
