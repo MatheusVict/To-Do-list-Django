693cf0f64567e501fb5d4f4b0aba5481ee4f4a65
Matheus Vict <matheusvictorhenrique@gmail.com>
2022-08-01 21:28:48 -0300

    psycopg2

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+release: python manage.py migrate
 web: gunicorn helloworld.wsgi --log-file -
--- helloworld/settings.py
+++ helloworld/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from telnetlib import LOGOUT
 import django_on_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1qu)c&%0a!r^1(4nf_0_n4!jg!n7^)-nqb7r$8g!_vwh&t%b=t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://to-do-list-matheus.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
@@ -83,10 +84,7 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':dj_database_url.config(default=f'sqlite:///{BASE_DIR}/db.sqlite3')
 }
 
 
